f1d3e00f777aff21714da294decf399e088ed7e1
Dominic Orme <dominic.orme@kineo.com>
2014-08-27 12:36:37 +0100

    Moved the image generators to their own objects. Moved any util functions to their own object too. Iterate works at 0.5 seconds per row. The original currently doesn't function

--- canvas.js
+++ canvas.js
@@ -1,20 +1,83 @@
 $(function() {
-    window.Image = {
+    if (typeof(window.Methods) === 'undefined') {
+        window.Methods = {};
+    }
+
+    window.Methods.Iterate = {
+        usedColours:[],
+        defaultValue:null,
+        previousValue:undefined,
+
+        init: function(canvas, defaultValue) {
+            this.defaultValue = defaultValue;
+        },
+
+        generate: function(i, j) {
+            var potential = {
+                'r':0,
+                'g':0,
+                'b':0,
+                'a':0
+            };
+            if (typeof(this.previousValue) !== "undefined") {
+                // This obviously isn't a potential option, but used to
+                // have a baseline to increment from
+                potential = window.ImageUtils.hexToRgb(this.previousValue);
+            } else {
+                // No previousValue has been defined - so we create on.
+                // It's also no in the list of used colours, so it'll
+                // go straight through.
+                this.previousValue = window.ImageUtils.rgbToHex(
+                    this.defaultValue.r,
+                    this.defaultValue.g,
+                    this.defaultValue.b
+                );
+            }
+
+            var possibleColour = this.previousValue;
+
+            while (this.usedColours.indexOf(possibleColour) >= 0) {
+                potential.r = potential.r + 1;
+                if (potential.r > 255) {
+                    potential.r = 0;
+                    potential.g = potential.g + 1;
+                    if (potential.g > 255) {
+                        potential.b = potential.b + 1;
+                    }
+                }
+                possibleColour = window.ImageUtils.rgbToHex(
+                    potential.r, potential.g, potential.b
+                );
+            }
+
+            this.previousValue = possibleColour;
+
+            return potential;
+        },
+
+        record: function(colour) {
+            var hexColour = window.ImageUtils.rgbToHex(colour.r, colour.g, colour.b);
+            this.usedColours.push(hexColour);
+        }
+    },
+
+    window.Methods.Original = {
         memoryCanvas: [],
-        usedColours: [],
-        defaultValue: {
-            'r':0,
-            'g':0,
-            'b':0,
-            'a':1 // 0: fully transparent -> 1: not transparent
+        usedColours:[],
+
+        init: function(canvas, defaultValue) {
+            var width = canvas.width;
+            var height = canvas.height;
+
+            for (var i = 0; i < width; i++) {
+                var row = [];
+                for (var j = 0; j < height; j++) {
+                    row.push(defaultValue);
+                }
+                this.memoryCanvas.push(row);
+            }
         },
-        componentToHex: function(c) {
-            var hex = c.toString(16);
-            return hex.length == 1 ? "0" + hex : hex;
-        },
-        rgbToHex: function(r, g, b) {
-            return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
-        },
+
         average: function(values) {
             var sum = values.reduceRight(function(x, total) {
                 return total + x;
@@ -22,16 +85,26 @@ $(function() {
 
             return sum/values.length;
         },
-        hexToRgb: function(hex) {
-            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-            return result ? {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16)
-            } : null;
+
+        _getSurroundingColours: function(i, j) {
+            var grid = [];
+
+            var previousI = i - 1 < 0 ? 0 : i - 1;
+            var previousJ = j - 1 < 0 ? 0 : j - 1;
+            for (var x = previousI; x < i + 1; x++) {
+                var row = [];
+                for (var y = previousJ; y < j + 1; y++) {
+                    row.push(this.memoryCanvas[x][y]);
+                }
+                grid.push(row);
+            }
+
+            return grid;
         },
-        _originalMethod: function(surroundingSquares) {
+
+        generate: function(i, j) {
             // surroundingSquares is an n x n grid (min n = 2, max n = 3)
+            var surroundingSquares = [];
             var reds = [];
             var greens = [];
             var blues = [];
@@ -40,7 +113,7 @@ $(function() {
                 for (var j = 0; j < row.length; j++) {
                     var colour = row[j];
                     if (colour !== this.defaultValue) {
-                        var colours = this.hexToRgb(colour);
+                        var colours = window.ImageUtils.hexToRgb(colour);
                         if (colours !== null) {
                             reds.push(colours.r);
                             greens.push(colours.g);
@@ -72,7 +145,7 @@ $(function() {
                 'a':1
             };
 
-            var possible = this.rgbToHex(potential.r, potential.g, potential.b);
+            var possible = window.ImageUtils.rgbToHex(potential.r, potential.g, potential.b);
 
             if (this.usedColours.indexOf(possible) >= 0) {
                 // Already used this colour
@@ -91,7 +164,7 @@ $(function() {
                                 potential.r = i;
                                 potential.g = j;
                                 potential.b = k;
-                                possible = this.rgbToHex(i, j, k);
+                                possible = window.ImageUtils.rgbToHex(i, j, k);
                                 if (this.usedColours.indexOf(possible) < 0) {
                                     found = true;
                                     break;
@@ -116,133 +189,115 @@ $(function() {
             return potential;
         },
 
-        _iterationMethod: function(surroundingSquares) {
-            var potential = {
-                'r':0,
-                'g':0,
-                'b':0,
-                'a':0
-            };
-            if (typeof(this.previousValue) !== "undefined") {
-                // This obviously isn't a potential option, but used to
-                // have a baseline to increment from
-                potential = this.hexToRgb(this.previousValue);
-            } else {
-                // No previousValue has been defined - so we create on.
-                // It's also no in the list of used colours, so it'll
-                // go straight through.
-                this.previousValue = this.rgbToHex(
-                    this.defaultValue.r,
-                    this.defaultValue.g,
-                    this.defaultValue.b
-                );
-            }
+        record: function(colour) {
+            var hexColour = window.ImageUtils.rgbToHex(colour.r, colour.g, colour.b);
+            this.usedColours.push(hexColour);
+        }
+    },
 
-            var possibleColour = this.previousValue;
-
-            while (this.usedColours.indexOf(possibleColour) >= 0) {
-                potential.r = potential.r + 1;
-                if (potential.r > 255) {
-                    potential.r = 0;
-                    potential.g = potential.g + 1;
-                    if (potential.g > 255) {
-                        potential.b = potential.b + 1;
-                    }
-                }
-                potential.a = Math.random();
-                possibleColour = this.rgbToHex(
-                    potential.r, potential.g, potential.b
-                );
-            }
-
-            return potential;
+    window.ImageUtils = {
+        componentToHex: function(c) {
+            var hex = c.toString(16);
+            return hex.length == 1 ? "0" + hex : hex;
         },
+        rgbToHex: function(r, g, b) {
+            return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
+        },
+
+        hexToRgb: function(hex) {
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            return result ? {
+                r: parseInt(result[1], 16),
+                g: parseInt(result[2], 16),
+                b: parseInt(result[3], 16)
+            } : null;
+        },
+        unique: function(values) {
+            console.log(
+                values.length + " of which unique: " + values.reduceRight(
+                    function(memo, num){
+                        if (memo.indexOf(num) === -1) {
+                            memo.push(num);
+                        }
+                        return memo;
+                    }, []
+                ).length
+            );
+        }
+    },
+
+    window.Image = {
+        uniqueCheck:false,
+        canvas:null,
+        context:null,
+
+        defaultValue: {
+            'r':0,
+            'g':0,
+            'b':0,
+            'a':1 // 0: fully transparent -> 1: not transparent
+        },
+        method:null,
 
         setMethod: function(method) {
             switch(method){
                 case "original":
                 case "ORIGINAL":
-                    this.generateColour = this._originalMethod;
+                    this.method = window.Methods.Original;
                     break;
                 case "iterate":
                 case "ITERATE":
-                    this.generateColour = this._iterationMethod
+                    this.method = window.Methods.Iterate;
                     break;
             }
         },
 
-        _getSurroundingColours: function(i, j) {
-            var grid = [];
+        init: function() {
+            this.canvas = $(document).find("#area")[0];
+            this.context = this.canvas.getContext('2d');
 
-            var previousI = i - 1 < 0 ? 0 : i - 1;
-            var previousJ = j - 1 < 0 ? 0 : j - 1;
-            for (var x = previousI; x < i + 1; x++) {
-                var row = [];
-                for (var y = previousJ; y < j + 1; y++) {
-                    row.push(this.memoryCanvas[x][y]);
-                }
-                grid.push(row);
-            }
-
-            return grid;
+            this.currentRow = 0;
+            this.method = window.Methods.Original;
         },
 
-        init: function() {
-            var canvas = $(document).find("#area")[0];
-            var context = canvas.getContext('2d');
-
-            var width = canvas.width;
-            var height = canvas.height;
-
-            for (var i = 0; i < width; i++) {
-                var row = [];
-                for (var j = 0; j < height; j++) {
-                    row.push(this.defaultValue);
-                }
-                this.memoryCanvas.push(row);
+        drawRow: function(row) {
+            for (var i = 0; i < row.length; i++) {
+                this.context.fillStyle = window.ImageUtils.rgbToHex(
+                    row[i].r, row[i].g, row[i].b
+                );
+                this.context.fillRect(i, this.currentRow, 1, 1);
             }
-            this.usedColours = [];
+            this.currentRow++;
         },
 
         begin: function() {
+            this.currentRow = 0;
+
             var method = $(document).find("#method option:selected").val();
             this.setMethod(method);
 
-            var canvas = $(document).find("#area")[0];
-            var context = canvas.getContext('2d');
+            this.method.init(this.canvas, this.defaultValue);
 
-            var width = canvas.width;
-            var height = canvas.height;
+            var width = this.canvas.width;
+            var height = this.canvas.height;
 
             // Iterate over the canvas
+            var now = Date.now();
             for (var i = 0; i < width; i++) {
                 var row = [];
                 for (var j = 0; j < height; j++) {
-                    var colour = this.generateColour(
-                        this._getSurroundingColours(i, j)
-                    );
-                    var hexColour = this.rgbToHex(colour.r, colour.g, colour.b);
-                    this.usedColours.push(hexColour);
-                    this.memoryCanvas[i][j] = colour;
-                    console.log(
-                        "generated colour " + hexColour + " for (" + i + "," + j + ")"
-                    );
-                }
-            }
+                    var colour = this.method.generate(i, j);
+                    this.method.record(colour);
 
-            for (var i = 0; i < width; i++) {
-                for (j = 0; j < height; j++) {
-                    context.globalAlpha(this.memoryCanvas[i][j].a)
-                    context.fillStyle = this.rgbToHex(
-                        this.memoryCanvas[i][j].r,
-                        this.memoryCanvas[i][j].g,
-                        this.memoryCanvas[i][j].b
-                    );
-                    context.fillRect(i, j, 1, 1);
+                    row.push(colour);
                 }
+                this.drawRow(row);
+                console.log("finished row " + i + " in: " + (Date.now() - now));
+                now = Date.now();
             }
-
-            this.init();
+            if (this.uniqueCheck === true) {
+                this.unique();
+            }
         }
     }
 

7391ba349a4d32b38382e97e2c081ba0790c3b55
Dominic Orme <dominic.orme@kineo.com>
2014-08-31 22:54:37 +0100

    Heatmaps based on the time it took for a pixel to be generated

--- canvas.js
+++ canvas.js
@@ -23,7 +23,10 @@ $(function() {
         // 2D context for the canvas object for the image
         context:null,
         // Last time a row or column was completed
-        lasttime: 0,
+        lastrowtime: 0,
+
+        heatmap:null,
+        heatmapContext:null,
 
         // Whether a worker is currently running ( stopping the 'begin' button
         // being clicked multiple times)
@@ -43,8 +46,28 @@ $(function() {
         // The currently running worker
         worker:null,
 
+        frequency: {
+            keys:{},
+            increment: function(x) {
+                if (x in this.keys) {
+                    this.keys[x]++;
+                } else {
+                    this.keys[x] = 0;
+                }
+            },
+
+            toArray: function() {
+                var response = [];
+                $.each(this.keys, function(key, value) {
+                    response.push([key, value]);
+                });
+
+                return response;
+            }
+        },
+
         setMethod: function(method) {
-            switch(method){
+            switch(method) {
                 case "original":
                 case "ORIGINAL":
                     this.worker = new Worker('original-generator.js');
@@ -60,26 +83,60 @@ $(function() {
                     );
                     break;
             }
+
+            this.heatmapWorker.terminate();
+            this.heatmapWorker = new Worker('heatmap.js');
+            this.heatmapWorker.addEventListener(
+                'message', $.proxy(this.drawHeatmap, this), false
+            );
         },
+
+        lasttime:null,
 
         draw: function(e) {
             if (e.data.running == true) {
                 var colour = e.data.colour;
                 var coords = e.data.coordinates;
 
+                var now = Date.now();
                 if (e.data.rowComplete == true) {
-                    var now = Date.now();
-                    this.plot(coords.x, (now - this.lasttime));
-                    this.lasttime = now;
+                    this.plot(coords.x, (now - this.lastrowtime));
+                    this.lastrowtime = now;
                 }
                 this.context.fillStyle = window.ImageUtils.rgbToHex(
                     colour.r, colour.g, colour.b
                 );
                 this.context.fillRect(coords.x, coords.y, 1, 1);
+                this.heatmapWorker.postMessage({
+                    'time':(now - this.lasttime),
+                    'coords':{
+                        'x':coords.x,
+                        'y':coords.y
+                    }
+                });
+                this.frequency.increment(now - this.lasttime);
+
+                this.lasttime = now;
             } else {
+                $.plot(
+                    $('#timefrequency'),
+                    [{
+                        'data':this.frequency.toArray(),
+                        'bars':{'show':true}
+                    }]
+                );
                 console.log("Finished at: " + Date.now());
                 this.running = false;
             }
+        },
+
+        drawHeatmap: function(e) {
+            var colour = e.data.colour;
+            var coords = e.data.coordinates;
+            this.heatmapContext.fillStyle = window.ImageUtils.rgbToHex(
+                colour.r, colour.g, colour.b
+            );
+            this.heatmapContext.fillRect(coords.x, coords.y, 1, 1);
         },
 
         plot: function(x, value) {
@@ -90,20 +147,51 @@ $(function() {
         init: function() {
             this.canvas = $(document).find("#area")[0];
             this.context = this.canvas.getContext('2d');
+
+            this.heatmap = $(document).find("#heatmap")[0];
+            this.heatmapContext = this.heatmap.getContext('2d');
+
+            this.heatmapexample = $(document).find("#heatmapexample")[0];
+            this.heatmapexampleContext = this.heatmapexample.getContext('2d');
+
+            this.heatmapWorker = new Worker('heatmap.js');
+            this.heatmapWorker.addEventListener(
+                'message', $.proxy(this.drawHeatmapExample, this), false
+            );
+
+            this.heatmapWorker.postMessage({
+                'displayFull':true
+            });
         },
+
+        heatmapExampleX:0,
+        drawHeatmapExample: function(e) {
+            var colour = e.data.colour;
+            var length = e.data.width;
+            $(document).find("#heatmapexample").css('width', length);
+            $(document).find("#timefrequency").css('width', length);
+            this.heatmapexampleContext.fillStyle = window.ImageUtils.rgbToHex(
+                colour.r, colour.g, colour.b
+            );
+            this.heatmapexampleContext.fillRect(this.heatmapExampleX, 0, 1, 1);
+            this.heatmapExampleX++;
+        },
+
+        heatmapWorker:null,
 
         begin: function() {
             if (this.running === true) {
                 return;
             }
 
-            $.plot($("#flotchart"), this.chartData);
-            
+            $.plot($("#flotchart"), [this.chartData]);
+
             var method = $(document).find("#method option:selected").val();
             this.setMethod(method);
 
+            this.lastrowtime = Date.now();
             this.lasttime = Date.now();
-            console.log("Started at: " + this.lasttime);
+            console.log("Started at: " + this.lastrowtime);
 
             this.worker.postMessage({
                 'canvas':{
@@ -113,11 +201,37 @@ $(function() {
                 'default':this.defaultValue
             });
             this.running = true;
+        },
+
+        testHeatmapWorker:null,
+        testHeatmapCanvas:null,
+        testHeatmapContext:null,
+        testHeatmap: function() {
+            this.testHeatmapWorker = new Worker('heatmap.js');
+            this.testHeatmapWorker.addEventListener(
+                'message', $.proxy(this.drawTestHeatmap, this), false
+            );
+            this.testHeatmapCanvas = $("#heatmaptestcanvas")[0];
+            this.testHeatmapContext = this.testHeatmapCanvas.getContext('2d');
+            this.testHeatmapWorker.postMessage({'test':true});
+        },
+
+        drawTestHeatmap: function(e) {
+            $(this.testHeatmapCanvas).css('width', e.data.width);
+            var colour = e.data.colour;
+            this.testHeatmapContext.fillStyle = window.ImageUtils.rgbToHex(
+                colour.r, colour.g, colour.b
+            );
+            this.testHeatmapContext.fillRect(e.data.x, 0, 1, 1);
         }
     }
 
     $(document).find("#start").on('click', function() {
         window.Image.begin();
+    });
+
+    $(document).find("#heatmaptest").on('click', function() {
+        window.Image.testHeatmap();
     });
 
     window.Image.init();
---
+++ heatmap.js
@@ -0,0 +1,106 @@
+function Point(r, g, b, a) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+
+    this.componentToHex = function(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    };
+    this.hash = function() {
+        return "#" + this.componentToHex(this.r) +
+                this.componentToHex(this.g) +
+                this.componentToHex(this.b) +
+                this.componentToHex(this.a);
+    };
+    this.toSimpleObject = function() {
+        return {
+            'r':this.r,
+            'g':this.g,
+            'b':this.b,
+            'a':this.a
+        };
+    };
+}
+
+var Map = {
+    colours:[],
+    coloursLength:0,
+
+    init: function() {
+        console.log("heat map initilized");
+        r = 0;
+        g = 0;
+        b = 0;
+
+        // First colour is white
+        this.colours.push(new Point(r, g, b));
+
+        var delta = 16;
+
+        // Then we go from very red to very green
+        for (var r = 255, g = 0; r >= 0; r -= delta, g += delta) {
+            var point = new Point(r, g, b);
+            console.log(JSON.stringify(point.toSimpleObject()));
+            this.colours.push(new Point(r, g, b));
+        }
+
+        r = 0;
+
+        // Then very green to very blue
+        for (var g = 255, b = 0; g >= 0; g -= delta, b += delta) {
+            var point = new Point(r, g, b);
+            console.log(JSON.stringify(point.toSimpleObject()));
+            this.colours.push(new Point(r, g, b));
+        }
+
+        g = 0;
+
+        // Then very blue back to very red
+        for (var b = 255, r = 0; b >= 0; b -= delta, r += delta) {
+            var point = new Point(r, g, b);
+            console.log(JSON.stringify(point.toSimpleObject()));
+            this.colours.push(new Point(r, g, b));
+        }
+
+        console.log("Using " + this.colours.length + " colours");
+        this.coloursLength = this.colours.length;
+    },
+
+    getColourForValue: function(x) {
+        var mod = x % (this.coloursLength);
+        return this.colours[mod];
+    }
+}
+Map.init();
+
+self.addEventListener('message', function(e) {
+    var time = e.data.time;
+    var coords = e.data.coords;
+
+    if (e.data.displayFull === true) {
+        for (var i = 0, length = Map.coloursLength; i < length; i++) {
+            self.postMessage({
+                'colour':Map.colours[i].toSimpleObject(),
+                'width':Map.coloursLength
+            });
+        }
+    } else if (e.data.test === true) {
+        for (var i = 0, length = Map.coloursLength * 2; i < length; i++) {
+            self.postMessage({
+                'colour':Map.getColourForValue(i).toSimpleObject(),
+                'width':Map.coloursLength * 2,
+                'x':i
+            });
+        }
+    } else {
+        self.postMessage({
+            'colour':Map.getColourForValue(time).toSimpleObject(),
+            'coordinates':{
+                'x':coords.x,
+                'y':coords.y
+            }
+        });
+    }
+});
